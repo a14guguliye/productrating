0acdc6e4e740eb93459ca1b356978510bab3f729
a14guguliye <quluquliyev08@gmail.com>
2023-07-31 15:37:53 -0700

    added new things

--- ProductRating/settings.py
+++ ProductRating/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-(wcxw-j9xvylo0mytru7-rlki54!b6#=i$*)9=j0=*rlk(4l9i"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
