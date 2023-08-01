2e55a60b97484deff64cc24b5e7c5c19fc861d14
a14guguliye <quluquliyev08@gmail.com>
2023-08-01 12:13:39 -0700

    added base user

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser,AbstractUser
 
 # Create your models here.
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractUser):
     def __str__(self) -> str:
         return self.get_username
