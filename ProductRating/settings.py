721752fe8b35b964055beceeee81d5b68a6c9d34
a14guguliye <quluquliyev08@gmail.com>
2023-08-01 11:25:52 -0700

    made changes on static

--- ProductRating/settings.py
+++ ProductRating/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -130,4 +130,5 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+STATIC_ROOT="/var/www/ProductRating/static/"
 
